e5cc95f098c98c9e6702f41fcce86ee436c0bfa9
Dax Ravi <d@ironbay.co>
2021-12-28 16:30:18 -0500

    Remove

--- packages/console/src/data/aws/cognito.ts
+++ packages/console/src/data/aws/cognito.ts
@@ -4,11 +4,6 @@ import {
 } from "@aws-sdk/client-cognito-identity-provider";
 import { useInfiniteQuery } from "react-query";
 import { useClient } from "./client";
-import { FetchHttpHandler } from "@aws-sdk/fetch-http-handler";
-
-const c = new CognitoIdentityProviderClient({
-  requestHandler: new FetchHttpHandler(),
-});
 
 export function useUsersQuery(pool: string) {
   const cognito = useClient(CognitoIdentityProviderClient);
