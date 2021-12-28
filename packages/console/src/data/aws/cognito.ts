1f53c164a5830de1cbfda90d812445f5d70ef219
Dax Ravi <d@ironbay.co>
2021-12-28 16:29:54 -0500

    Proxy all aws-sdk requests through CLI

--- packages/console/src/data/aws/cognito.ts
+++ packages/console/src/data/aws/cognito.ts
@@ -4,6 +4,11 @@ import {
 } from "@aws-sdk/client-cognito-identity-provider";
 import { useInfiniteQuery } from "react-query";
 import { useClient } from "./client";
+import { FetchHttpHandler } from "@aws-sdk/fetch-http-handler";
+
+const c = new CognitoIdentityProviderClient({
+  requestHandler: new FetchHttpHandler(),
+});
 
 export function useUsersQuery(pool: string) {
   const cognito = useClient(CognitoIdentityProviderClient);
